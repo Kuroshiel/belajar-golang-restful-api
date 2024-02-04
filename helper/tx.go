fbe32dec6470d1f42967373917f8fcc30ab12736
Kuroshiel <alifianfikry@gmail.com>
2024-02-04 20:22:52 +0700

    Category Service Implementation Golang RESTful API

---
+++ helper/tx.go
@@ -0,0 +1,17 @@
+package helper
+
+import "database/sql"
+
+// Category Service Implementation Golang RESTful API
+
+func CommitOrRollback(tx *sql.Tx) {
+	err := recover()
+	if err != nil {
+		errorRollback := tx.Rollback()
+		PanicIfError(errorRollback)
+		panic(err)
+	} else {
+		errorCommit := tx.Commit()
+		PanicIfError(errorCommit)
+	}
+}
